d3aa7e0dd86a179e59f3ce87a1a0c7c59b1a48ab
LashKirui <141770037+LashKirui@users.noreply.github.com>
2023-10-26 11:15:40 +0300

    changing dictionary values

---
+++ dict_prac.py
@@ -0,0 +1,10 @@
+my_dict = {
+    "dictionary": "contains all the definitions of words",
+    "book": "write or read on it",
+    "age": "the number of years you have lived on earth",
+}
+
+for key in my_dict:
+    print(key)
+    print(my_dict[key])
+
